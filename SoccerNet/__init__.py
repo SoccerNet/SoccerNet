aadc0ca5d8c5bd3d8a3611e29fbddb3b455ebfad
Silvio Giancola <silvio.giancola@gmail.com>
2023-08-29 18:44:37 +0300

    update github link

--- SoccerNet/__init__.py
+++ SoccerNet/__init__.py
@@ -3,4 +3,4 @@ __version__ = '.'.join(__version_info__)
 __authors__ = "Silvio Giancola"
 __authors_username__ = "giancos"
 __author_email__ = "silvio.giancola@kaust.edu.sa"
-__github__ = 'https://github.com/SilvioGiancola/SoccerNetv2'
+__github__ = 'https://github.com/SoccerNet/SoccerNet'
